812dd6abdcefebbf49bb232967e152a16058f64f
Kevin Nguyen <72723199+kevinhoangnn@users.noreply.github.com>
2022-10-05 12:05:58 -0700

    start on main

--- RPGGame/main.py
+++ RPGGame/main.py
@@ -1,10 +1,15 @@
 import json
+import bigBoss
+import soldier
+
+with open('RPGGame\gameconfigs.json', 'r') as f:
+    data = json.loads(f.read())
+
+bossStats = data['boss']
+weaponStats = data['weapon']
+soldierStats = data['soldier']
 
 if __name__ == '__main__':
-    path = 'RPGGame\gameconfigs.json'
+    
 
-    with open(path, 'r') as f:
-        data = json.loads(f.read())
-
-    bossStats = data['boss']
-    print(bossStats['boss_m_hp'])
+    bossMan = bigBoss.BigBoss(bossStats['boss_p_hp'], bossStats['boss_m_hp'], bossStats['boss_b_hp'])
