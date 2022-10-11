f194f877b67f2b2feda6e89e2ed3065ca9706025
Gilbert <castellanos.gilbert@hotmail.com>
2022-10-11 09:56:06 -0700

    email retrieval progress

--- RMA/returnly_auto.py
+++ RMA/returnly_auto.py
@@ -1,3 +1,5 @@
+from tkinter.ttk import Progressbar
+import pandas as pd
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
@@ -25,8 +27,10 @@ def exportReport():
     interactor.send_keys(userReturnly)
     interactor = driver.find_element(By.ID, 'user_password')
     interactor.send_keys(passReturnly)
-    interactor = driver.find_element(By.XPATH, '//*[@id="new_user"]/div[3]/div/input')
-    interactor.click()
+    select =  WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="new_user"]/div[3]/div/input')))
+    select.click()
+    print('Login succesful!')
 
     #Navigating to reports tab and exporting
     select = driver.find_element(By.XPATH, '//*[@id="sidebar"]/nav/ul/li[3]/a/span')
@@ -36,8 +40,49 @@ def exportReport():
     select.click()
     select = WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.ID, 'js-reporting-modal-submit')))
-    select.click()
+    print('Download succesful report has been sent to email!')
+    driver.quit()
 
+def retrieveReport():
+    domain = 'unisco.com'
+    userEmail = ''
+    passEmail = ''
+
+
+    #Retrieving report from email
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://login.microsoftonline.com/")
+    select = driver.find_element(By.CLASS_NAME, 'table-cell text-left content')
+    select.click()
+    interactor = driver.find_element(By.XPATH, '//*[@id="credentialList"]/div[3]/div[1]/div/div[2]')
+    interactor.send_keys(domain)
+
+def formatReport():
+
+    file = "C:\\Users\\gcastellanos\\Desktop\\Flag_&_Anthem_Returnly_Return_Details_created_at_2022-09-20_2022-09-20_PDT.csv"
+
+    df = pd.read_csv(file)
+
+    df = df[['RMA', 'Original Order', 'Tracking Number', 'Shopper Email', 'Shipped From Name', 'Shipped From Address 1', 'Shipped From City', 'Shipped From State',
+             'Shipped From Zip', 'Shipped From Country Code', 'Barcode']]
+
+    newColDict = {'ClientID':'FLAANT0001', 'Reverse Type':'Consumer Return', 'Phone':'', 'RMA Expiration Date':'', 'Ship Method':'Small Parcel', 'Shipment Carrier':'USPS',
+                  'BOL':'', 'ETA':'', 'Note':'', 'UPC':'', 'Buyer ID':'', 'Return Qty':1, 'UOM':'EA', 'Serial Number':''}
+
+    newColKeys = newColDict.keys()
+
+    for index, colName in enumerate(newColKeys):
+        df[colName] = newColDict[colName]
+
+    df = df.rename(columns={'Original Order' : 'Reference', 'Shipped From Name' : 'Return Party', 'Shipped From Address 1' : 'Return From Address 1',
+                            'Shipped From City' : 'Return From City', 'Shipped From State' : 'Return From State', 'Shipped From Zip' : 'Return From Postal Code', 
+                            'Shipped From Country Code' : 'Return From Country', 'Shopper Email' : 'Email', 'Tracking Number' : 'Pro / Tracking Number', 
+                            'Barcode' : 'Item Name'})
+                    
+    df['Reference'] = df['Reference'].str.replace('#', '')
+
+    df.to_excel('Output.xlsx', index=False)
 
 launchBrowser()
 exportReport()
+retrieveReport()
