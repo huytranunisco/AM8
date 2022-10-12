4bc920e01cec044c1c9d3240a0325888309c9735
Gilbert <castellanos.gilbert@hotmail.com>
2022-10-12 10:22:35 -0700

    RMA email portion update

--- RMA/returnly_auto.py
+++ RMA/returnly_auto.py
@@ -1,5 +1,9 @@
-import pandas as pd
+import os
+import webbrowser
 import imaplib
+import email
+import pandas as pd
+from email.header import decode_header
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
@@ -44,14 +48,17 @@ def exportReport():
         select.click()
         select = WebDriverWait(driver, 5).until(
             EC.presence_of_element_located((By.ID, 'js-reporting-modal-submit')))
-        print('Download succesful report has been sent to email!')
+        print('Download succesful, report has been sent to email!')
         driver.quit()
     except Exception as e:
         print('Failed to export report\n', 'Error: ', e)
         driver.quit()
 
+def clean(text):
+    # clean text for creating a folder
+    return "".join(c if c.isalnum() else "_" for c in text)
+
 def retrieveReport():
-    domain = 'unisco.com'
     userEmail = 'xxx'
     userPass = 'xxx'
 
@@ -59,18 +66,79 @@ def retrieveReport():
     imap = imaplib.IMAP4_SSL(imap_server)
     imap.login(userEmail, userPass)
 
-"""
-    #Logging in to email
-    try:
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        driver.get("https://login.microsoftonline.com/")
-        select = driver.find_element(By.C, '//*[@id="lightboxTemplateContainer"]/div[2]/div[1]/div/div/div/div/div[2]/div/div/div/div/div[2]') 
-        select.click()
-        interactor = driver.find_element(By.XPATH, '//*[@id="credentialList"]/div[3]/div[1]/div/div[2]')
-        interactor.send_keys(domain)
-    except Exception as e:
-        print('Failed to login\n', 'Error: ', e)
-"""
+    status, messages = imap.select('INBOX')
+    N = 1
+    messages = int(messages[0])
+
+    for i in range(messages, messages-N, -1):
+        # fetch the email message by ID
+        res, msg = imap.fetch(str(i), "(RFC822)")
+        for response in msg:
+            if isinstance(response, tuple):
+                # parse a bytes email into a message object
+                msg = email.message_from_bytes(response[1])
+                # decode the email subject
+                subject, encoding = decode_header(msg["Subject"])[0]
+                if isinstance(subject, bytes):
+                    # if it's a bytes, decode to str
+                    subject = subject.decode(encoding)
+                # decode email sender
+                From, encoding = decode_header(msg.get("From"))[0]
+                if isinstance(From, bytes):
+                    From = From.decode(encoding)
+                print("Subject:", subject)
+                print("From:", From)
+                # if the email message is multipart
+                if msg.is_multipart():
+                    # iterate over email parts
+                    for part in msg.walk():
+                        # extract content type of email
+                        content_type = part.get_content_type()
+                        content_disposition = str(part.get("Content-Disposition"))
+                        try:
+                            # get the email body
+                            body = part.get_payload(decode=True).decode()
+                        except:
+                            pass
+                        if content_type == "text/plain" and "attachment" not in content_disposition:
+                            # print text/plain emails and skip attachments
+                            print(body)
+                        elif "attachment" in content_disposition:
+                            # download attachment
+                            filename = part.get_filename()
+                            if filename:
+                                folder_name = clean(subject)
+                                if not os.path.isdir(folder_name):
+                                    # make a folder for this email (named after the subject)
+                                    os.mkdir(folder_name)
+                                filepath = os.path.join(folder_name, filename)
+                                # download attachment and save it
+                                open(filepath, "wb").write(part.get_payload(decode=True))
+                else:
+                    # extract content type of email
+                    content_type = msg.get_content_type()
+                    # get the email body
+                    body = msg.get_payload(decode=True).decode()
+                    if content_type == "text/plain":
+                        # print only text email parts
+                        print(body)
+                if content_type == "text/html":
+                    # if it's HTML, create a new HTML file and open it in browser
+                    folder_name = clean(subject)
+                    if not os.path.isdir(folder_name):
+                        # make a folder for this email (named after the subject)
+                        os.mkdir(folder_name)
+                    filename = "index.html"
+                    filepath = os.path.join(folder_name, filename)
+                    # write the file
+                    open(filepath, "w").write(body)
+                    # open in the default browser
+                    webbrowser.open(filepath)
+                print("="*100)
+    # close the connection and logout
+    imap.close()
+    imap.logout()
+
 
 def formatReport():
 
