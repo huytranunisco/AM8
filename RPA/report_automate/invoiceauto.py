af453c1a409db9902786172c204b99e45db844e1
Kevin Nguyen <72723199+kevinhoangnn@users.noreply.github.com>
2022-10-17 10:44:01 -0700

    Updating paths for remote

--- RPA/report_automate/invoiceauto.py
+++ RPA/report_automate/invoiceauto.py
@@ -1,11 +1,10 @@
-from sqlite3 import DatabaseError
-from symbol import yield_arg
 import BNPauto
 from datetime import date, timedelta
-from calendar import monthrange, week
 import os.path
 from os import mkdir
 from pandas import read_excel
+from calendar import monthrange
+from shutil import copy
 
 '''
 accountdict = {'Jetson': ['Valley View', 'JETSON(JETSON)'], 
@@ -44,17 +43,22 @@ accountdict = {'Jetson': ['Valley View', 'JETSON(JETSON)'],
                'SCHC Wilson Art': ['New Jersey', 'SCHC - Wilsonart(SCHC - Wilsonart)'],
                'TCU Trading': ['GREENWOOD', 'TCU TRADING LTD(TCU TRADING LTD)'], 'Tytus Grill': ['Valley View', 'TYTUS GRILLS, LLC(TYTUS GRILLS, LLC)']}
 '''
-def getInvoice(accBNP, facility, startP, endP, accName, dateName):
+def getInvoice(accBNP, facility, startP, endP, accName, dateName, cycle):
     try:  
         facility, invoiceName = BNPauto.exportHandle(accBNP, facility, startP, endP, accName)
 
         if facility == False or invoiceName == False: 
             return False
         
-        newName = accName + '_' + fac
-        newPath = folderName + '//' + 'Invoice[' + newName + ']_' + dateName + '.xlsx'
+        newName = accName + '-' + fac + '-' + cycle + '-Invoice'
+        newPath = '\\Accounts\\00 - Historical Invoices\\' + newName + '-' + dateName + '.xlsx'
+        copyPath = '\\Accounts\\02 - Current Invoices\\' + newName + '-' + dateName + '.xlsx'
 
         os.rename(invoiceName, newPath)
+
+        copy(newPath, copyPath)
+
+        print('Done!')
 
     except Exception as e:
         if hasattr(e, 'message'):
@@ -77,34 +81,44 @@ try:
 except (FileExistsError):
     print('Invoice folder already exists. Continuing...')
 
-invoiceAccs = read_excel('RPA\\report_automate\\BNP Excel Sheet.xlsx', sheet_name='Account_Fac_Freq')
+invoiceAccs = read_excel('BNP Excel Sheet.xlsx', sheet_name='Account_Fac_Freq')
 
 for index in invoiceAccs.index:
     fac = invoiceAccs['Facility Name'][index]
     bnpName = invoiceAccs['AccountName'][index]
     accName = bnpName
 
+    print(f'Getting Invoice for {accName} ({fac})...')
+
     if invoiceAccs['BillingFreq'][index] == 'Bimonthly':
         #Assuming is ran on 1st and 16th
+        if today.day < 16:
+            previousMonth = today.month - 1 if today.month != 1 else 12
+            previousYear = today.year - 1 if today.month == 12 else today.year
+            startBimonthly = date(previousYear, previousMonth, 16)
+            endBimonthly = date(previousYear, previousMonth, monthrange(previousYear, previousMonth)[1])
+        elif today.day >= 16:
+            startBimonthly = date(today.year, today.month, 1)
+            endBimonthly = date(today.year, today.month, 15)
+        '''
         startBimonthly = today + timedelta(weeks=-2, days=-1)
         endBimonthly = startBimonthly + timedelta(weeks=+2)
+        ''' 
         startPeriod = startBimonthly.strftime("%m/%d/%y")
         endPeriod = endBimonthly.strftime("%m/%d/%y")
         dateName = startBimonthly.strftime("%m-%d-%y")
 
-        print(f'Start: {startPeriod}, End: {endPeriod}')
-
-        getInvoice(bnpName, fac, startPeriod, endPeriod, accName, dateName)
+        getInvoice(bnpName, fac, startPeriod, endPeriod, accName, dateName, 'Bimonthly')
 
     elif invoiceAccs['BillingFreq'][index] == 'Weekly':
-        if today.day == 1:
+        if today.day < 16:
             previousMonth = today.month - 1 if today.month != 1 else 12
             previousYear = today.year - 1 if today.month == 12 else today.year
             previousBillDate = date(previousYear, previousMonth, 16)
-        elif today.day == 16:
+            weeklyStartPeriods = find_sundays_between(previousBillDate, date(previousYear, previousMonth, monthrange(previousYear, previousMonth)[1]))
+        elif today.day >= 16:
             previousBillDate = date(today.year, today.month, 1)
-
-        weeklyStartPeriods = find_sundays_between(previousBillDate, today)
+            weeklyStartPeriods = find_sundays_between(previousBillDate, date(today.year, today.month, 15))
 
         for d in weeklyStartPeriods:
             endWeekly = d + timedelta(days=+6)
@@ -114,7 +128,7 @@ for index in invoiceAccs.index:
 
             print(f'Start: {startPeriod}, End: {endPeriod}')
 
-            getInvoice(bnpName, fac, startPeriod, endPeriod, accName, dateName)
+            getInvoice(bnpName, fac, startPeriod, endPeriod, accName, dateName, 'Weekly')
 
     else: continue
 
