d1b2d62047dbd722541d57566278c3d1d108681d
Gilbert <castellanos.gilbert@hotmail.com>
2022-10-19 12:24:22 -0700

    working email api

--- RMA/returnly_auto.py
+++ RMA/returnly_auto.py
@@ -4,8 +4,8 @@ from email.parser import BytesParser
 import pandas as pd
 import imaplib
 import getpass
-from bs4 import BeautifulSoup
-import urllib.request
+import os
+import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
@@ -14,14 +14,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 def exportReport():
+    global userReturnly
+    global passReturnly
+
     print('Enter password for Returnly!')
     userReturnly = 'FLAANT0001.rms@unisco.com'
-    passReturnly = getpass.getpass()
+    passReturnly = 'Syst0002' #getpass.getpass()
 
     #Logging in to returnly
     try:
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        driver.get("https://dashboard.returnly.com/dashboard/users/login")
+        driver.get('https://dashboard.returnly.com/dashboard/users/login')
         interactor = driver.find_element(By.ID, 'user_email') 
         interactor.send_keys(userReturnly)
         interactor = driver.find_element(By.ID, 'user_password')
@@ -50,49 +53,96 @@ def exportReport():
     except Exception as e:
         print('Failed to export report\n','Error: ', e)
         driver.quit()
-        md
 
 def downloadReport():
     domain = 'webmail.unisco.com'
     userEmail = 'FLAANT0001.rms@unisco.com'
-    userPass = getpass.getpass()
-    folder = input('Please enter location you want to download report (remove ""): ')
+    userPass = 'Syst0001' #getpass.getpass()
+    folder = input('Please enter the folder location you want to download the report to (remove ""): ')
+
+    while os.path.exists(folder) != True:
+        folder = input('Please enter a valid folder location...: ')
 
     try:
         M = imaplib.IMAP4_SSL(domain)
         M.login(userEmail, userPass)
         
-        M.SELECT('INBOX')
-        rv, data = M.search(None, 'Unseen')
-        if rv == False:
-            print("No report found yet, please wait!")
-            pass
-        
+        while True:
+            M.SELECT('INBOX')
+            rv, data = M.search(None, 'Unseen')
+            print(rv, data)
+            if rv != 'OK':
+                print("No report found yet... please wait...")
+                time.sleep(10)
+                rv, data = M.search(None, 'Unseen')
+                if rv == 'OK':
+                    print('Report has been found... continuing...')
+                    break
+            else:
+                print('Report has been found... continuing...')
+                break       
+
         for num in data[0].split():
-            rv, data = M.fetch(num, '(RFC822)')
-            if rv == False:
-                print("ERROR getting message"), num
-                return
-            print("Writing message "), num
+            while True:
+                rv, data = M.fetch(num, '(RFC822)')
+                if rv != 'OK':
+                    print("Error downloading the email... Please make sure that the email is 'unread' and it is in the inbox!"), num
+                    quit()
+                else:
+                    break
+            print('Downloading the email...'), num
             f = open('%s/%s.eml' %(folder, num), 'wb')
             f.write(data[0][1])
             f.close()
 
-        file = input('Please enter report location (remove ""): ')
+        file = input('Please enter report location with file name at end (remove "", filepath/b"12".eml): ')
 
+        messageList = []
         with open(file) as email_file:
             email_message = email.message_from_file(email_file)
         if email_message.is_multipart():
             for part in email_message.walk():
                 message = str(part.get_payload(decode=True))
-                print(message)
+                messageList.append(message)
+
+        a = messageList[1]
+        newText = ""
+        urlFlag = 0
+        linksList = []
+
+        for i in range(len(a)):
+            if(a[i]=='<'):
+                urlFlag = 1
+                continue
+            if(a[i]=='>'):
+                urlFlag = 0
+                linksList.append(newText)
+                newText = ''
+                continue
+            if(urlFlag==1):
+                newText = newText + a[i]
+
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.get('https://dashboard.returnly.com/dashboard/users/login')
+        interactor = driver.find_element(By.ID, 'user_email') 
+        interactor.send_keys('FLAANT0001.rms@unisco.com')
+        interactor = driver.find_element(By.ID, 'user_password')
+        interactor.send_keys('Syst0002')
+        print('Attempting to login...')
+        select =  WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="new_user"]/div[3]/div/input')))
+        select.click()
+        print('Succesfully logged in!')
+        driver.get(linksList[0])
+        time.sleep(5)
+        driver.quit()
 
     except Exception as e:
         print('Error: ', e)
 
 def formatReport():
 
-    file = input('Please enter location of report (remove ""): ')
+    file = input('Please enter location of the downloaded report (remove ""): ')
     
     df = pd.read_csv(file)
 
