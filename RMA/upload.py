62d96b831e27b51e9064ace2907f2b0183e45699
Gilbert <castellanos.gilbert@hotmail.com>
2022-11-10 11:58:50 -0800

    Upload progress
    Need to figure out how to switch frames in selenium so I can select the "Import RMA" button

--- RMA/upload.py
+++ RMA/upload.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,8 +8,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
-
 
 def Upload(file):
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -25,8 +24,22 @@ def Upload(file):
     element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="loginBtn"]/button')))
     actions.move_to_element(element).click().perform()
 
+    time.sleep(3)
+    driver.get('https://stage.logisticsteam.com/#/rms/returnManagement/rma')
+    driver.maximize_window()
 
+    driver.switch_to().frame(1)
 
+    element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.LINK_TEXT, 'Import RMA')))
+    actions.move_to_element(element).click().perform()
+
+    #element = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, ""xpath=//a[@onclick='ToImportRMA()']")))
+    #actions.move_to_element(element).click().perform()
+
+    #uploadelement = webdriverwait(driver, 20).until(ec.element_to_be_clickable((by.xpath, "(xpath=//input[@id='files']"))));
+    #uploadelement.send_keys('c:\\users\\gcastellanos\\downloads\\transformed_11-03-2022 10-31-52_pdt.xlsx')
+    
+    
 if __name__ == '__main__':
     try:
         user = os.getlogin()
@@ -37,4 +50,3 @@ if __name__ == '__main__':
         Upload('a')
     except Exception as e:
         print(e)
-        os.system('pause')
