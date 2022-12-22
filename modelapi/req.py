aec3a3d533525ad346a1291f56c4b8dbab363b9b
Kevin Nguyen <72723199+kevinhoangnn@users.noreply.github.com>
2022-12-22 12:32:43 -0800

    update model

--- modelapi/req.py
+++ modelapi/req.py
@@ -5,8 +5,6 @@ import pandas as pd
 url = 'http://127.0.0.1:5000/model'
 
 dict = {'ID': {'2': 3}, 'n1': {'2': 3}, 'n2': {'2':3}}
-r = requests.post(url, json=dict)
-print(r.status_code)
 
 r = requests.get(url)
 print(r.json())
