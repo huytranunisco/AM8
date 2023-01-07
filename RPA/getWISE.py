9c43c8be3fd4768cfc98330d34ef07c98eb8f264
Kevin Nguyen <72723199+kevinhoangnn@users.noreply.github.com>
2023-01-07 13:00:46 -0800

    completed grabbing activities reports

--- RPA/getWISE.py
+++ RPA/getWISE.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from datetime import date, timedelta
+from dateutil.relativedelta import relativedelta
 import os
 from glob import glob
 from shutil import move
@@ -15,29 +16,16 @@ from selenium.webdriver import ActionChains
 import time
 
 if __name__ == '__main__':
-    downloaddir = 'C:\\Users\\' + os.getlogin() + '\\Downloads'
-    accsdir = 'C:\\Users\\' + os.getlogin() +'Desktop\\Valley View - Wise Activity Reports 2022\\Jan'
-
     customers = pd.read_excel('customerlist.xlsx', sheet_name='Sheet1')
 
     customers = customers['Customer'].to_list()
 
     fac = 'Valley View'
 
-    start = date(2022, 1, 1)
-    end = date(2022, start.month + 1, 1) + timedelta(days=-1)
-    if start.month == 12:
-        end = date(2022, 12, 31)
-
-    startdate = start.strftime('%Y-%m-%d')
-    enddate = end.strftime('%Y-%m-%d')
-
     with open('accountconfigs.json', 'r') as f:
         data = loads(f.read())
 
     userDownloadPath = "C:\\Users\\" + os.getlogin() + "\\Downloads\\*.xlsx"
-    downloadFolderBefore = glob(userDownloadPath)
-
     wisebots = pd.read_excel('BNP Excel Sheet.xlsx', sheet_name='Facility list')
     facilityList = wisebots['FacilityName'].to_list()
 
@@ -84,10 +72,27 @@ if __name__ == '__main__':
     interactor = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div1/div[2]/div[1]/ul/li[2]/ul/li[2]/a/span')))
     action.move_to_element(interactor).click().perform()
 
+    start = date(2022, 12, 16)
+    end = date(2022, 12, 31)
+
+        
+    startdate = start.strftime('%Y-%m-%d')
+    enddate = end.strftime('%Y-%m-%d')
+
+    interactor = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div[2]/div/div/div/div/div/div/div/div/div/div[2]/form/div[1]/div[3]/lt-date-time/div/input')))
+    action.click(interactor).send_keys(startdate).perform()
+    interactor = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div[2]/div/div/div/div/div/div/div/div/div/div[2]/form/div[1]/div[4]/lt-date-time/div/input')))
+    action.click(interactor).send_keys(enddate).perform()
+
     for acc in customers:
+        #if customers.index(acc) < 33:
+        #    continue
+
+        print(acc)
+
         #Inputting Customer, Time From, Time To
         customer = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div[2]/div/div/div/div/div/div/div/div/div/div[2]/form/div[1]/div[1]/organization-auto-complete/div/div')))
-        action.click(customer).perform()
+        action.click(customer)
         time.sleep(2)
         for letter in acc:
             time.sleep(0.05)
@@ -96,13 +101,32 @@ if __name__ == '__main__':
         time.sleep(4)
         action.send_keys(Keys.ENTER).perform()
 
-        interactor = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div[2]/div/div/div/div/div/div/div/div/div/div[2]/form/div[1]/div[3]/lt-date-time/div/input')))
-        action.click(interactor).send_keys(startdate).perform()
-        interactor = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[1]/div[2]/div[2]/div/div/div/div/div/div/div/div/div/div[2]/form/div[1]/div[4]/lt-date-time/div/input')))
-        action.click(interactor).send_keys(enddate).perform()
-
         #Exporting
         interactor = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[2]/div/div/div/div/div/div/div/div/div/div[2]/form/div[2]/div[1]/input')
         action.move_to_element(interactor).click().perform()
         interactor = driver.find_element(By.XPATH, '//*[@id="sitecontent"]/div/div/div/div[2]/form/div[2]/div[2]/unis-waitting-btn/button')
         action.move_to_element(interactor).click().perform()
+    
+        downloadWait = True
+        downloadFolderBefore = glob(userDownloadPath)
+
+        skip = False
+
+        while downloadWait:
+            if skip == True:
+                print('Skipped')
+                break
+
+            downloadFolderAfter = glob(userDownloadPath)
+
+            if len(downloadFolderBefore) < len(downloadFolderAfter):
+                downloadWait = False
+
+            time.sleep(1)
+
+            try:
+                interactor = driver.find_element(By.XPATH, '/html/body/div[7]/md-dialog/md-dialog-actions/button')
+                interactor.click()
+                skip = True
+            except:
+                pass
